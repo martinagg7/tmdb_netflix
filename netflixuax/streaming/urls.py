645060586c2f6edcf90b9bf0b0a54104273d25a4
Martina Garcia <martinagg@myuax.com>
2024-12-03 13:53:54 +0100

    +organizado

--- netflixuax/streaming/urls.py
+++ netflixuax/streaming/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.inicio, name='inicio'), 
-    path('now_playing/', views.now_playing_view, name='now_playing'),
-    path('top_rated/', views.top_rated_view, name='top_rated'),
-    path('detalle/<int:pelicula_id>/', views.detalle, name='detalle')
+    path('', views.inicio, name='inicio'),
+    path('pelicula/now-playing/', views.now_playing_view, name='now_playing'),
+    path('pelicula/top-rated/', views.top_rated_view, name='top_rated'),
+    path('pelicula/detalle/<int:pelicula_id>/', views.detalle, name='detalle'),
 ]
--- netflixuax/streaming/views.py
+++ netflixuax/streaming/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from .scripts.peliculas import peliculas_populares,now_playing,top_rated,detalle_pelicula
+from .scripts.peliculas import peliculas_populares, now_playing, top_rated, detalle_pelicula
 
-#para la página incio peliculas populares
+
 def inicio(request):
-    peliculas = peliculas_populares()  
-    return render(request, 'streaming/inicio.html', {'peliculas': peliculas})
+    peliculas = peliculas_populares()
+    return render(request, 'streaming/peliculas/now_playing.html', {'peliculas': peliculas})
 
+# PARA PELICULAS
 def now_playing_view(request):
     peliculas = now_playing()
-    return render(request, 'streaming/now_playing.html', {'peliculas': peliculas})
+    return render(request, 'streaming/peliculas/now_playing.html', {'peliculas': peliculas})
 
 def top_rated_view(request):
     peliculas = top_rated()
-    return render(request, 'streaming/top_rated.html', {'peliculas': peliculas})
+    return render(request, 'streaming/peliculas/top_rated.html', {'peliculas': peliculas})
 
 def detalle(request, pelicula_id):
     detalles = detalle_pelicula(pelicula_id)
-    return render(request, 'streaming/detalle.html', {'detalles': detalles})
+    return render(request, 'streaming/peliculas/detalle.html', {'detalles': detalles})
